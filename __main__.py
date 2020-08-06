2904b99c64e0680fe6ac3cfd62c5a45de7c00361
Virusmater <virusmater@gmail.com>
2020-08-06 11:51:54 +0200

    update

--- __main__.py
+++ __main__.py
@@ -9,22 +9,28 @@ namespace = {'w': "http://schemas.openxmlformats.org/wordprocessingml/2006/main"
 
 
 for file in glob.glob("resources/*.docx"):
-    print("Checking:", file)
+    print("Next;")
+
     p = Document(file)
     core_properties = p.core_properties
-    print("Created on", core_properties.created)
+    # print("Created on", core_properties.created)
     cpart = p.part.part_related_by(RT.COMMENTS)
     xmlString = cpart.blob
     root = etree.fromstring(xmlString)
-    print("Comments are from:")
+    # print("Comments are on:")
+    print("file\tdate\tauthor\ttext")
     comments = []
     for comment in root.xpath("w:comment", namespaces=namespace):
         date = comment.xpath('@w:date', namespaces=namespace)[0]
-        print(date)
+        author = comment.xpath('@w:author', namespaces=namespace)[0]
+        text_node = comment.xpath('w:p/w:r[*]/w:t', namespaces=namespace)
+        if text_node:
+            text = text_node[0].text
+            print(file, date, author, text, sep="\t")
         comments.append(parse(date).replace(tzinfo=None))
     oldest = min(comments)
     newest = max(comments)
-    print("Oldest:", oldest)
-    print("Newest:", newest)
-    print("Time passed between first and last comment:", newest - oldest)
-    print("Time passed creation and last comment:", newest - core_properties.created)
+    print("Oldest comment\t", oldest)
+    print("Newest comment\t", newest)
+    print("Time between\t", newest - oldest)
+    # print("Time passed creation and last comment;", newest - core_properties.created)
